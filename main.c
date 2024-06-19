b103ed533d6eda9e140520cf657112e54c5476de
Kelvin963 <sousaks054@gmail.com>
2024-06-19 14:56:16 -0300

    MAIN

--- main.c
+++ main.c
@@ -3,10 +3,24 @@
 #include "hanoi.h"
 
 int main(){
+    Pilha A, B, C;
+    int n;
+    char origem, destino;
 
+    printf("Digite o numero de discos: ");
+    scanf("%d", &n);
+    system("cls");
 
+    while(n > MAX_DISCOS){
+        printf("Numero maximo de discos eh %d\n", MAX_DISCOS);
+        printf("Digite o numero de discos: ");
+        scanf("%d", &n);
+        system("cls");
+    }
 
-
+    inicializarPilha(&A);
+    inicializarPilha(&B);
+    inicializarPilha(&C);
 
 
 
